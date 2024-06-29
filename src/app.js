1e455b1f46037479a489f6a16808e50d322beddc
Ankit Singh Chandel <ankitlucky1016@gmail.com>
2024-06-29 14:05:53 +0530

    wrote some controllers

--- src/app.js
+++ src/app.js
@@ -14,5 +14,14 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
+
+//routes
+
+import userRouter from "./routes/user.routes.js"
+
+//declaring routes
+
+ app.use("/api/v1/users",userRouter)
 
 export {app}
