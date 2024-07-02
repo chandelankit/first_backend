65c4b29ac7be584a5d08a7e078eb79711cc4ff13
Ankit Singh Chandel <ankitlucky1016@gmail.com>
2024-07-02 22:49:18 +0530

    register controller bux fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler(async(req,res)=>{
 
     }
 
-    const existingUser = User.findOne({$or:[{username},{email}]})
+    const existingUser = await User.findOne({$or:[{username},{email}]})
 
     if(existingUser){
 
@@ -26,7 +26,12 @@ const registerUser = asyncHandler(async(req,res)=>{
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"avatar is required")
@@ -36,7 +41,7 @@ const registerUser = asyncHandler(async(req,res)=>{
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar){
-        throw new ApiError(400,"Avatar is required cloudinary upload")
+        throw new ApiError(400,"Avatar is required")
     }
 
     const user = await User.create({
